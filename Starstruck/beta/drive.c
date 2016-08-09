c442649bc12e166a35d05e12e010b89c8189a62c
Cameron Lund <cameronlund4@gmail.com>
2016-08-08 20:32:52 -0400

    Cleanup in drive.c
    Cleans up and adds comments, reorders to make finding specific methods
    easier

--- Starstruck/beta/drive.c
+++ Starstruck/beta/drive.c
@@ -1,8 +1,12 @@
-
-
 #define DRIVE_THRESHOLD_FORWARD 20 // Joystick forward threshold
 #define DRIVE_THRESHOLD_TURN 20 // Joystick turn threshold
 #define DRIVE_THRESHOLD_STRAFE 20 // Joystick strafe threshold
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                        Normal drive methods (No sensors)
+//
+/////////////////////////////////////////////////////////////////////////////////////////
 
 // Cleaner way of setting all four drive motor speeds.
 // Drive with checks to rule out errors with joystick controls
@@ -18,24 +22,17 @@ void driveRaw(int speedFL, int speedBL, int speedFR, int speedBR) {
 	motor[driveBR] = speedBR; // Back right
 }
 
-void clearDriveEncoders() {
-	nMotorEncoder[driveFR] = 0;
-	nMotorEncoder[driveFL] = 0;
-	nMotorEncoder[driveBL]= 0;
-	nMotorEncoder[driveBR] = 0;
-}
-
 // Set mecanum wheel motor speeds based on forward, turn, and strafe speeds
 // PARAMETERS:
 //	int: -127 to 127, speed to drive forward or backward respectively
 //	int: -127 to 127, speed to turn left or right respectively
 //	int: -127 to 127, speed to strafe left or right respectively
 void drive(int speedForward, int speedTurn, int speedStrafe) {
-	// Not using driveRaw, easier to read this in this case
-	motor[driveFL] = speedForward + speedTurn + speedStrafe;
-	motor[driveBL] = speedForward + speedTurn - speedStrafe;
-	motor[driveFR] = speedForward - speedTurn - speedStrafe;
-	motor[driveBR] = speedForward - speedTurn + speedStrafe;
+	int speedFL = speedForward + speedTurn + speedStrafe;
+	int speedBL = speedForward + speedTurn - speedStrafe;
+	int speedFR = speedForward - speedTurn - speedStrafe;
+	int speedBR = speedForward - speedTurn + speedStrafe;
+	driveRaw(speedFL, speedBL, speedFR, speedBR);
 }
 
 // Drive with checks to rule out errors with joystick controls
@@ -69,23 +66,42 @@ void driveWithLogic(int speedForward, int speedTurn, int speedStrafe) {
 	driveWithLogic(speedForward, speedTurn, speedStrafe, 1.0, 1.0, 1.0);
 }
 
-// Considers the input as if the front of the robot was at a different angle (For example, considering
-// -> the right side of the robot to be the front, or the front right corner to be the front) all while
-// -> facing the same direction so you can move towards a scoring object and launch stuff over the fence
-// CRS: Correction of Rotation Stick (or crazy rotational stuff)
-// Beta, will add documentation when complete
-void driveWithCRS(int speedForward, int speedStrafe, float startDegree, float currentDegree) {
-	if (speedForward <= DRIVE_THRESHOLD_FORWARD) speedForward = 0;
-	if (speedStrafe <= DRIVE_THRESHOLD_STRAFE) speedStrafe = 0;
-	float degree = degreeToRad(currentDegree-startDegree);
-	// Considering speed as x, x' = y*sin(a) + x*cos(a)
-	speedForward = speedStrafe*sin(degree) + speedForward*cos(degree);
-	// Considering strafe as y, y' = y*cos(a) - x*sin(a)
-	speedStrafe = speedStrafe*cos(degree) - speedForward*sin(degree);
+// Set each side of the drive to a certain speed.
+// PARAMETERS:
+//  int: How fast to move the left side (-127 to 127)
+//  int: How fast to move the right side (-127 to 127)
+void driveTank(int speedLeft, int speedRight){
+	driveRaw(speedLeft,speedLeft,speedRight,speedRight);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                        Smart drive methods (Encoders & PID)
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Sets the encoders on all of our drive motors back to 0.
+void clearDriveEncoders() {
+	nMotorEncoder[driveFR] = 0;
+	nMotorEncoder[driveFL] = 0;
+	nMotorEncoder[driveBL]= 0;
+	nMotorEncoder[driveBR] = 0;
+}
+
+// Used to set the tick targets, because tasks can't take parameters
 long ticksFrontLeft, ticksBackLeft, ticksFrontRight, ticksBackRight = 0;
 
+// Clean way to set above motor targets
+// PARAMETERS:
+//  int: Target ticks to change for front left motor
+//  int: Target ticks to change for back left motor
+//  int: Target ticks to change for front right motor
+//  int: Target ticks to change for back right motor
+// NOTE:
+//  - You should enter the delta, the method will figure out the total
+//  - For example, if you want it to go forward 300 and the encoder is already
+//    at 1000, you should still only put in 300.
+//  - Negative = backwards wheel movement.
 void setupMotorTicks(long frontl, long backl, long frontr, long backr) {
 	ticksFrontLeft = frontl + nMotorEncoder[driveFL];
 	ticksBackLeft = backl + nMotorEncoder[driveBL];
@@ -93,6 +109,13 @@ void setupMotorTicks(long frontl, long backl, long frontr, long backr) {
 	ticksBackRight = backr + nMotorEncoder[driveBR];
 }
 
+// Drive tick method. Controls left and right side useful for point turns.
+// Reads encoder valte of the front motors for each side.
+// PARAMETERS:
+//  int: How fast to move the left side drive (-127 to 127)
+//  long: How far to spin the left drive, in ticks. If above speed is negative, so should this number.
+//  int: How fast to move the right side drive (-127 to 127)
+//  long: How far to spin the right drive, in ticks. If above speed is negative, so should this number.
 void driveTilRightLeft(int speedLeft, long ticksLeft, int speedRight, long ticksRight) {
 	setupMotorTicks(ticksLeft, ticksLeft, ticksRight, ticksRight);
 	// While we're not where we want to be (Ternary allows for both forward and backward input)
@@ -102,48 +125,25 @@ void driveTilRightLeft(int speedLeft, long ticksLeft, int speedRight, long ticks
 	}
 }
 
-void driveTank(int speedLeft, int speedRight){
-	driveRaw(speedLeft,speedLeft,speedRight,speedRight);
-}
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                        CRS (Currently on the backburner)
+//
+/////////////////////////////////////////////////////////////////////////////////////////
 
+// Considers the input as if the front of the robot was at a different angle (For example, considering
+// -> the right side of the robot to be the front, or the front right corner to be the front) all while
+// -> facing the same direction so you can move towards a scoring object and launch stuff over the fence
+// CRS: Correction of Rotation Stick (or crazy rotational stuff)
+// Beta, will add documentation when complete
 /*
-int x2 = 0, y1 = 0, x1 = 0, threshold = 15;
-
-void driveWithLogic(int speedForward, int speedTurn, int speedStrafe, float forwardMultiplier, float speedMultiplier, float strafeMultiplier) {
-	// Driver motion control
-	float driveMultiplier = -1;
-	float turnMultiplier = -1.5;
-	if (vexRT[Btn6D]) {
-		driveMultiplier = -0.25;
-		turnMultiplier =  -0.75;
-		} else if (vexRT[Btn6U]) {
-		driveMultiplier = -0.5;
-		turnMultiplier = -1;
-	}
-	if(abs(vexRT[Ch3]) > threshold)
-		y1 = floor(((float) vexRT[Ch3]) * driveMultiplier);
-	else
-		y1 = 0;
-	if (abs(vexRT[Ch4]) > threshold)
-		x1 = floor(((float) vexRT[Ch4]) * driveMultiplier);
-	else
-		x1 = 0;
-	if (abs(vexRT[Ch1]) > threshold)
-		x2 = floor(((float) vexRT[Ch1]) * turnMultiplier);
-	else
-		x2 = 0;
-
-	motor[driveFR] = y1 - x2 - x1;
-	motor[driveBR] = y1-x2 + x1;
-	motor[driveFL] = y1 + x2 + x1;
-	motor[driveBL] = y1 + x2 - x1;
-}
-
-void driveWithCRS(int speedForward, int speedStrafe, float startDegree, float currentDegree){
-
-}
-
-void drive(int speedForward, int speedTurn, int speedStrafe) {
-
+void driveWithCRS(int speedForward, int speedStrafe, float startDegree, float currentDegree) {
+	if (speedForward <= DRIVE_THRESHOLD_FORWARD) speedForward = 0;
+	if (speedStrafe <= DRIVE_THRESHOLD_STRAFE) speedStrafe = 0;
+	float degree = degreeToRad(currentDegree-startDegree);
+	// Considering speed as x, x' = y*sin(a) + x*cos(a)
+	speedForward = speedStrafe*sin(degree) + speedForward*cos(degree);
+	// Considering strafe as y, y' = y*cos(a) - x*sin(a)
+	speedStrafe = speedStrafe*cos(degree) - speedForward*sin(degree);
 }
 */
