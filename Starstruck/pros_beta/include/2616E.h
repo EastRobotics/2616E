27f71399d1212393361171364dfb9e343d257ebc
Cameron Lund <cameronlund4@gmail.com>
2016-12-11 11:32:04 -0500

    Start of moving our lib to Pros

---
+++ Starstruck/pros_beta/include/2616E.h
@@ -0,0 +1,16 @@
+#ifndef LIB2616E_H_
+
+// This prevents multiple inclusion, which isn't bad for this file but is good practice
+#define LIB2616E_H_
+
+// Allow usage of this file in C++ programs
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// End C++ export structure
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Starstruck/pros_beta/src/opcontrol.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * Runs the user operator control code. This function will be started in its own task with the
+ * default priority and stack size whenever the robot is enabled via the Field Management System
+ * or the VEX Competition Switch in the operator control mode. If the robot is disabled or
+ * communications is lost, the operator control task will be stopped by the kernel. Re-enabling
+ * the robot will restart the task, not resume it from where it left off.
+ *
+ * If no VEX Competition Switch or Field Management system is plugged in, the VEX Cortex will
+ * run the operator control task. Be warned that this will also occur if the VEX Cortex is
+ * tethered directly to a computer via the USB A to A cable without any VEX Joystick attached.
+ *
+ * Code running in this task can take almost any action, as the VEX Joystick is available and
+ * the scheduler is operational. However, proper use of delay() or taskDelayUntil() is highly
+ * recommended to give other tasks (including system tasks such as updating LCDs) time to run.
+ *
+ * This task should never exit; it should end with some kind of infinite loop, even if empty.
+ */
+void operatorControl() {
+	while (true) { // true > 1
+		delay(20);
+	}
+}
