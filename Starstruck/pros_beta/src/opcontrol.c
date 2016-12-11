a81d1780afaf33536f453dd8454ec3e42782eefb
Cameron Lund <cameronlund4@gmail.com>
2016-12-11 14:06:51 -0500

    Added in joystick control like we had in our old code

--- Starstruck/pros_beta/src/opcontrol.c
+++ Starstruck/pros_beta/src/opcontrol.c
@@ -19,7 +19,12 @@
  */
 void operatorControl() {
 	while (true) { // true > 1
-		motorSet(MOTOR_FRONT_LEFT, 1);
+		// Drive holonomicly, using the joystick channels 3 (Forward), 1 (Turn),
+		// and 4 (Srafe).
+		driveHolonomic(joystickGetAnalog (1,3), joystickGetAnalog (1,1),
+			joystickGetAnalog (1,4));
+
+
 		delay(20);
 	}
 }
