b99f4ea1e18d6255d97babeb97c52eadc721476d
Cameron Lund <cameronlund4@gmail.com>
2017-01-09 17:26:17 -0500

    More auto changes

--- Starstruck/beta/auton.c
+++ Starstruck/beta/auton.c
@@ -96,6 +96,68 @@ void tempEncoderPoint(int speed, int ticks){
 	}
 	driveRaw(0,0,0,0);
 }
+int clawTargetLeft;
+int clawTargetRight;
+bool clawRunning = false;
+int clawSpeed;
+
+task adjustClawPosition() {
+	clawRunning = true;
+	int clawPosRight;
+	int clawPosLeft;
+	const int misal = 30;
+	do { // Align the claws
+		clawPosLeft = getMotorEncoder(clawL);
+		clawPosRight = getMotorEncoder(clawR);
+		clawPosRight *= -1; // so they both count up as they go out
+		// If we're misaligning, counteract
+		// Left claw
+		if (abs(clawPosLeft-clawTargetLeft) > misal) {
+			motor[clawL] = clawPosLeft-clawTargetLeft > 0 ? -1 * clawSpeed : clawSpeed;
+			} else {
+			motor[clawL] = 0;
+		}
+		// Right claw
+		if (abs(clawPosRight-clawTargetRight) > misal) {
+			motor[clawR] = clawPosRight-clawTargetRight > 0 ? clawSpeed : -1 * clawSpeed;
+			} else {
+			motor[clawR] = 0;
+		}
+		wait1Msec(20);
+	} while // They aren't aliged
+		(!(abs(clawPosRight-clawTargetRight) > misal) // Right claw not aligned
+	&& !(abs(clawPosLeft-clawTargetLeft) > misal)); // Left claw not aligned
+	clawRunning = false;
+}
+
+
+void stopClaw() {
+	stopTask(	adjustClawPosition );
+}
+
+// Should really only be used for opening, see clawClose(int ms)
+void setClaw(int target, int speed) {
+	clawSpeed = speed;
+	clawTargetLeft = target;
+	clawTargetRight = target;
+}
+
+// Used for when we want to clamp an unknown orientation, and can't guarentee a certain close value
+void clawClose(int ms, int speed) {
+	setClaw(0, speed); // Try and close the claw
+	int timeGone = 0;
+	while (timeGone < ms || pidRunning) { // While we still have time or haven't reached 0ms
+		wait1Msec(10);
+		timeGone += 10;
+	}
+	stopClaw();
+}
+
+// Wait for the claw to reach it's target
+void waitForClaw() {
+	while (clawRunning)
+		wait1Msec(20);
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -157,13 +219,14 @@ void runAuton() {
 		// TODO Figure out how long it takes to place stuff
 		int placementTimeStars = 3000;
 		int placementTimeCubes = 3000; // Might be 0, since robot goes and comes
+		int dumpValue = 300;
 
 		/*
 		** Green: Back up for stars and wait for placement
 		*/
 		pidDriveStraightLimit(-600,3000);
-		// TODO Deploy claw
-		wait1Msec(placementTimeStars); // Wait to place stars
+		/* TODO Change values */ setClaw(500,100); // Deploy claw
+		/* TODO Delete */ breakpoint();
 
 		/*
 		** Red: Go to fence and score stars
@@ -173,20 +236,34 @@ void runAuton() {
 		/* TODO Change values */ int fenceTurn = 600;
 		// Drive, dump, repeat
 		for (int i=0; i < 3; i++) { // Stars, then 2 cubes
+			wait1Msec(i == 0 ? placementTimeStars : placementTimeCubes); // Wait to place load
+			waitForClaw(); // Make sure claw is ready
+			/* TODO Change values */ clawClose(1000,100); // Clamp load for 1 second
+			/* TODO Delete */ breakpoint();
 			// Drive up to fence
 			pidDriveStraightLimit(-1*fenceDistance,10000);
 			wait1Msec(cooldownTime); // Motor cooldown
+			/* TODO Delete */ breakpoint();
+
+			// TODO Lift load so it doesn't drag?
 
 			// Turn towards fence
 			pidDrivePointLimit(-1*fenceTurn*sideMult,5000);
+			/* TODO Delete */ breakpoint();
 
-			// TODO Lift and then drop stars
+			// TODO Lift
+			/* TODO Change values */ setClaw(dumpValue,127); // Drop the load, open just enough to drop
+			waitForClaw();
+			/* TODO Delete */ breakpoint();
 			// TODO start lowering lift
 
 			if(i < 2) { // If we want to go back to starting tile
+				/* TODO Change values */ setClaw(500,127); // Set claw at whatever we want for pickup
 				// Turn away from fence
+				/* TODO Delete */ breakpoint();
 				pidDrivePointLimit(fenceTurn*sideMult,5000);
 				wait1Msec(cooldownTime); // Motor cooldown
+				/* TODO Delete */ breakpoint();
 				pidDriveStraight(fenceDistance); // Drive back to tile
 				// TODO Check and adjust claw values
 				waitForPidLimit(8000); // Make sure we wait for PID
@@ -197,21 +274,28 @@ void runAuton() {
 		** Yellow: Get 3 fence stars and dump
 		** (TODO Replace with gyro PID)
 		*/
-		// TODO Check and adjust claw values
+		/* TODO Change values */ setClaw(400,127); // Set claw at grabbing pos
+		/* TODO Delete */ breakpoint();
 		// After dumping, turn towards yellow stars
 		/* TODO Change values */ pidDrivePointLimit(90*sideMult,5000);
+		/* TODO Delete */ breakpoint();
 
 		// Drive into the stars
 		/* TODO Change values */ pidDriveStraightLimit(-1*600,10000);
+		/* TODO Delete */ breakpoint();
 
-		// TODO Close claw
+		/* TODO Change values */ clawClose(1000,100); // Clamp load for 1 second
+		/* TODO Delete */ breakpoint();
 
 		// Turn to fence to dump
 		/* TODO Change values */ pidDrivePointLimit(-90*sideMult,5000);
+		/* TODO Delete */ breakpoint();
 
 		// TODO Lift
 
-		// TODO Release and dump
+		/* TODO Change values */ setClaw(dumpValue,127); // Drop the load, open just enough to drop
+		waitForClaw();
+		/* TODO Delete */ breakpoint();
 
 		// TODO Lower lift
 
