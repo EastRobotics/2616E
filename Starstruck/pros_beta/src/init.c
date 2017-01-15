6c2dedd508efdab8d7ae0a190dd4081e114f543e
Cameron Lund <cameronlund4@gmail.com>
2017-01-15 16:03:00 -0500

    Moved LCD implementation to a new file
    ...finally. No more scrolling.

--- Starstruck/pros_beta/src/init.c
+++ Starstruck/pros_beta/src/init.c
@@ -52,172 +52,6 @@ void initDriveEncoders() {
 void initializeIO() {
 }
 
-void updateLCD(bool userCaused, int page) {
-  lcdClear(uart2);
-  switch (page) {
-    // [Page 1] Auton mode selector --------------------------------------------
-    case 1:
-      {
-        lcdPrintTitle("AutonMode");
-        const char* autonName;
-        switch (getAutonMode()) {
-          case 1:
-            autonName = "Do Nothing";
-            break;
-          case 2:
-            autonName = "Max Points";
-            break;
-          default: // Unknown mode
-            autonName = "Unknown";
-        }
-        char temp[16];
-        sprintf(temp,"%c %s %c",0xBC, autonName, 0xBB);
-        //lcdPrint(uart2, 2, "%c %s %c",0xBC, autonName, 0xBB);
-        lcdPrintCentered(temp,2);
-      }
-      break;
-
-      // [Page 2] Auton position selector --------------------------------------
-      case 2:
-        {
-          lcdPrintTitle("AutonPos");
-          char temp[16];
-          sprintf(temp,"%c %s %c",0xBC, getAutonPosition() ? "Right"
-          : "Left", 0xBB);
-          lcdPrintCentered(temp,2);
-        }
-        break;
-
-      // [Page 3] Auton color selector -----------------------------------------
-      case 3:
-        {
-          lcdPrintTitle("AutonColor");
-          char temp[16];
-          sprintf(temp,"%c %s %c",0xBC, getAutonColor() ? "Red"
-          : "Blue", 0xBB);
-          lcdPrintCentered(temp,2);
-        }
-        break;
-
-      // [Page 4] Home page ----------------------------------------------------
-      case 4:
-        {
-          lcdPrint(uart2, 1, "%cEssential Evil%c",0xCD,0xCD);
-          char batWarning = ' ';
-          // If really bad
-          if (batteryStatus(voltLevelExpander(ANALOG_POW_EXPAND, true)) >= 2 ||
-            batteryStatus(voltLevelMain()) >= 2) {
-            batWarning = 0xF5; // Noticeable down arrow thing
-          // If pretty bad
-          }
-          else if (batteryStatus(voltLevelExpander(ANALOG_POW_EXPAND, true))
-            >= 1 || batteryStatus(voltLevelMain()) >= 1) {
-            batWarning = 0x21;
-          }
-          lcdPrint(uart2, 2,"%cbat | aut%d",batWarning,getAutonMode());
-        }
-        break;
-
-      // [Page 5] Battery Stat -------------------------------------------------
-      case 5:
-        {
-          lcdPrint(uart2, 1, "%s%c B:%.2fv", "BatStat", 0xF6, voltLevelBackup());
-          lcdPrint(uart2, 2, " M:%.2fv E:%.2fv", voltLevelMain(),
-            voltLevelExpander(ANALOG_POW_EXPAND, true));
-        }
-        break;
-      // [Page 6] Claw pots ----------------------------------------------------
-      case 6:
-        {
-          lcdPrintTitle("ClawPots");
-          int clawPosLeft = encoderGet(clawL);
-      		int clawPosRight = encoderGet(clawR);
-          lcdPrint(uart2, 2, "L:%.4d R:%.4d", clawPosLeft, clawPosRight);
-        }
-        break;
-      // [Page 7] Line sensors -------------------------------------------------
-      case 7:
-        {
-          lcdPrintTitle("LineSensors");
-          lcdPrint(uart2, 2, "L: %.4d R: %.4d", analogRead(ANALOG_LINE_LEFT),
-          analogRead(ANALOG_LINE_RIGHT));
-        }
-        break;
-      // [Page 8] Arm pot ------------------------------------------------------
-      case 8:
-        {
-          lcdPrintTitle("ArmPot");
-          lcdPrint(uart2, 2, "Arm: %.4d", analogRead(ANALOG_POT_LIFT));
-        }
-        break;
-    // [Page ?] Unknown page ---------------------------------------------------
-    default:
-      lcdPrint(uart2, 1, "Page? %d",page);
-      break;
-    // -------------------------------------------------------------------------
-  }
-}
-
-void menuNext(int page) {
-  switch (page) {
-    // [Page 1] Auton mode selector --------------------------------------------
-    case 1:
-      {
-        int nextMode = getAutonMode()+1;
-        if (nextMode > getAutonModeMax())
-          setAutonMode(getAutonModeMin());
-        else
-          setAutonMode(nextMode);
-      }
-      break;
-    // [Page 2] Auton position selector --------------------------------------
-    case 2:
-      {
-        setAutonPosition(!getAutonPosition());
-      }
-      break;
-    // [Page 3] Auton color selector -------------------------------------------
-    case 3:
-      {
-        setAutonColor(!getAutonColor());
-      }
-      break;
-    // -------------------------------------------------------------------------
-  }
-  updateLCD(true, page);
-  lcdResetAutoRefresh();
-}
-
-void menuBack(int page) {
-  switch (page) {
-    // [Page 1] Auton mode selector --------------------------------------------
-    case 1:
-      {
-        int nextMode = getAutonMode()-1;
-        if (nextMode < getAutonModeMin())
-          setAutonMode(getAutonModeMax());
-        else
-          setAutonMode(nextMode);
-      }
-      break;
-    // [Page 2] Auton position selector --------------------------------------
-    case 2:
-      {
-        setAutonPosition(!getAutonPosition());
-      }
-      break;
-    // [Page 3] Auton color selector -------------------------------------------
-    case 3:
-      {
-        setAutonColor(!getAutonColor());
-      }
-      break;
-    // -------------------------------------------------------------------------
-  }
-  updateLCD(true, page);
-  lcdResetAutoRefresh();
-}
-
 /*
  * Runs user initialization code. This function will be started in its own task with the default
  * priority and stack size once when the robot is starting up. It is possible that the VEXnet
@@ -237,9 +71,9 @@ void initialize() {
   // Set up the LCD and start it
   print("[Init] Setting up the LCD\n");
   lcdInitMenu(1,8,4); // Min 1, max 8, home 4
-  lcdSetUpdater(updateLCD);
-  lcdSetMenuBack(menuBack);
-  lcdSetMenuNext(menuNext);
+  lcdSetUpdater(implUpdateLCD);
+  lcdSetMenuBack(implMenuBack);
+  lcdSetMenuNext(implMenuNext);
 
   // Set up our drive
   print("[Init] Setting up drive motors\n");
@@ -259,7 +93,7 @@ void initialize() {
   gyro = gyroInit(ANALOG_GYRO, 0); // 0 multiplier = default, not * 0
   delay(2000); // Give gyro some setup time
 
-  // Set up our integrated incoders
+  // Set up our encoders
   print("[Init] Setting up encoders\n");
   lcdSetText(uart2, 1, "Init Encs...");
   initDriveEncoders();
@@ -267,7 +101,6 @@ void initialize() {
   clawR = encoderInit(DIGITAL_ENC_CLAW_R_TOP,DIGITAL_ENC_CLAW_R_BOT,false);
   // Init left encoder, not reverse
   clawL = encoderInit(DIGITAL_ENC_CLAW_L_TOP,DIGITAL_ENC_CLAW_L_BOT,false);
-
 
   // Done init
   print("[Init] Finished, starting LCD menu\n");
