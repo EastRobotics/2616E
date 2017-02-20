fd5faaf877ba1605031dd75f66ea8086d1decfd7
Cameron Lund <cameronlund4@gmail.com>
2017-02-19 19:04:40 -0500

    Disable strafing for drive

--- Starstruck/pros_beta/src/opcontrol.c
+++ Starstruck/pros_beta/src/opcontrol.c
@@ -9,10 +9,10 @@ void operatorControl() {
 	shutdownPID(); // Make sure auton PID isn't running
 
 	while (true) { // true cooler than 1
-		// Drive holonomicly, using the joystick channels 3 (Forward), 1 (Turn),
-		// and 4 (Srafe).
+		// Drive normally, using the joystick channels 3 (Forward), 1 (Turn),
+		// and 0 for strafe
 		driveHolonomicWithLogic(joystickGetAnalog (1,3), joystickGetAnalog (1,1),
-			joystickGetAnalog (1,4));
+				0);
 
 		// Move the lift, using the buttons 5U (Up), 5D (Down), and 7D(Manual down)
 		if (joystickGetDigital(1, 5, JOY_UP) ||
